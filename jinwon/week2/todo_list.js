2c981dc09f1e8c9477102a4db3a392328e419589
Jinwon Lee <a67841586@gmail.com>
2023-07-17 21:15:17 +0900

    job done

--- jinwon/week2/todo_list.js
+++ jinwon/week2/todo_list.js
@@ -1,70 +1,67 @@
-function deleted() {
-  var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  
-  checkboxes.forEach(function(checkbox) {
-    if(checkbox.checked==true){
+function list_Making() {
+    var inputText = document.getElementById('to_Do').value;
+    // 입력된 텍스트 값 가져오기
+    
+    if (inputText.trim()=== "") {
+        return;
+        }
+    // 입력된 값 없으면 실행 X
 
-      var listItem = checkbox.parentNode;
-      listItem.parentNode.removeChild(listItem);
-      
+    else{
+        var listItem = `<ul id='todo_list'>` + `<input type='checkbox' id='checkbox'><input id='things' type='text' readOnly value="${to_Do.value}"><button class='click' onclick='alter()'>수정</button> <button class='click' onclick='save()'>저장</button>`;
+    // 리스트 아이템 HTML 생성
+
+    document.getElementById('list').innerHTML += listItem;
+    // 리스트 생성
+
+    document.getElementById("to_Do").value = ""; // 입력 필드 비우기
+
     }
-  });
+    }
+function alter() {
+    const things = document.querySelectorAll('#things');
+    const checkboxes = document.querySelectorAll('#checkbox');
+    let i =0;
+    for(i=0; i<things.length; i++){
+        if(checkboxes[i].checked===true){
+        things[i].readOnly=false;
+        }
+    }
+    }
+    // 수정기능 추가
+    function save() {
+        const things = document.querySelectorAll('#things');
+        const checkboxes = document.querySelectorAll('#checkbox');
+        let i =0;
+        for(i=0; i<things.length; i++){
+            if(checkboxes[i].checked===true){
+                things[i].readOnly=true;
+                checkboxes[i].checked=false;
+            }
+        }
+    }
+    // 저장기능 추가
+    function deleted() {
+        var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+            
+        checkboxes.forEach(function(checkbox) {
+            checkbox.parentNode.remove();
+        });
+    }
+    //선택삭제 기능 추가
+    function unselectAll() {
+        var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        
+        checkboxes.forEach(function(checkbox) {
+        checkbox.checked = false;
+        });
+    }
+    // 선택해제 기능 추가
 
-
-}
-
-function selectAll() {
-  var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  
-  checkboxes.forEach(function(checkbox) {
+    function selectAll() {
+        var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach(function(checkbox) {
     checkbox.checked = true;
-  });
+    });
+// 전체선택 기능 추가
 }
-
-function unselectAll() {
-  var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  
-  checkboxes.forEach(function(checkbox) {
-    checkbox.checked = false;
-  });
-}
-
-
-
-function listmaking() {
-    // 입력한 할 일 가져오기
-var to_do = document.getElementById("todolist").value;
-
-  // 문자열에 공백을 제거한 후에 아무것도 입력되지 않았을때는 true가 되어 return값을 통해 함수 종료
-    if (to_do.trim() === "") {
-    return;
-    }
-
-    // 새로운 <li> 요소 생성
-var newItem = document.createElement("text");
-
-
-  // 체크박스 추가
-var checkbox = document.createElement("input");
-checkbox.type = "checkbox";
-newItem.appendChild(checkbox);
-
-  // 할 일 받아오기
-var textNode = document.createTextNode(to_do);
-newItem.appendChild(textNode);
-
-    // 목록에 추가
-var list = document.getElementById("list");
-list.appendChild(newItem);
-
-  // 줄 바꿈 추가
-var lineBreak = document.createElement("br");
-newItem.appendChild(lineBreak);
-
-    // 입력 필드 비우기
-document.getElementById("todolist").value = "";
-
-
-
-  }
-
